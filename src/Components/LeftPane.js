c1e13ac21140cd9a7557142e5b19b56386c5819a
JenesesD <jenesesd@gmail.com>
2020-02-27 09:40:12 +0100

    filtering hero images

--- src/Components/LeftPane.js
+++ src/Components/LeftPane.js
@@ -7,7 +7,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import MyDecks from "./MyDecks";
 import CardList from "./CardList";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
@@ -24,11 +24,11 @@ export default function LeftPane() {
   const { cards } = useContext(CardsContext);
 
   return (
-    <Grid item xs={8}>
+    <Grid item xs={10}>
       <Paper className={classes.paper}>
         <div>
-          <Route path='/' exact component={() => <CardList cards={cards} />} />
-          <Route path='/mydecks' component={() => <MyDecks />} />
+          <Route path="/" exact component={() => <CardList cards={cards} />} />
+          <Route path="/mydecks" component={() => <MyDecks />} />
         </div>
       </Paper>
     </Grid>
--- src/Components/RightPane.js
+++ src/Components/RightPane.js
@@ -5,7 +5,7 @@ import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import CroppedCardList from "./CroppedCardList";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
@@ -24,10 +24,10 @@ export default function RightPane() {
   const classes = useStyles();
 
   return (
-    <Grid item xs={4}>
+    <Grid item xs={2}>
       <Paper className={classes.paper}>
         <div>
-          <Route path='/' exact component={CroppedCardList} />
+          <Route path="/" exact component={CroppedCardList} />
         </div>
       </Paper>
     </Grid>
