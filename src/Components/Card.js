97f00851f15e640c4a805593c2612eb1980522f4
JenesesD <jenesesd@gmail.com>
2020-02-27 15:01:35 +0100

    RightPane styling

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,12 +3,17 @@ import { DeckContext } from "../DataStore/DeckContext";
 
 export default function Card(props) {
   const [cardsInDeck, setCardsInDeck] = useContext(DeckContext);
+
+  const cardSize = {
+    height: "395px",
+    width: "295px"
+  };
   return (
     <img
-      onClick={() => {
-        setCardsInDeck([...cardsInDeck, props.card]);
-        console.log(cardsInDeck);
-      }}
+      // onClick={() => {
+      //   setCardsInDeck([...cardsInDeck, props.card]);
+      // }}
+      style={cardSize}
       id={props.card.id}
       src={props.card.image}
       alt=""
--- src/Components/CroppedCard.js
+++ src/Components/CroppedCard.js
@@ -1,33 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
+import { DeckContext } from "../DataStore/DeckContext";
 
 export default function CroppedCard(props) {
+  const [cardsInDeck, setCardsInDeck] = useContext(DeckContext);
   const container = {
-    position: "relative"
+    position: "relative",
+    marginBottom: "5px"
   };
 
   const nameStyle = {
     position: "absolute",
-    top: "50%",
-    left: "20%",
+    top: "28%",
+    left: "26%",
     color: "white",
     fontFamily: "URW Chancery L, cursive",
-    fontSize: "20px",
+    fontSize: "1.5rem",
     fontStyle: "oblique",
-    textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black"
+    textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black",
+    wordBreak: "break-word",
+    fontWeight: "bolder"
   };
 
   const manaCrystalStyle = {
     position: "absolute",
-    top: "-5px",
-    left: "0"
+    top: "-7px",
+    left: "-20px",
+    height: "90px",
+    width: "90px"
   };
 
   const manaStyle = {
+    textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black",
+    fontFamily: "URW Chancery L, cursive",
+    fontStyle: "oblique",
     position: "absolute",
-    left: "30px",
-    top: "10px",
-    fontSize: "40px",
+    left: "12px",
+    fontSize: "60px",
     color: "white"
+  };
+
+  const asd = {
+    width: "100%",
+    maxWidth: "100%",
+    borderRadius: "20px 10px 10px 20px"
+  };
+
+  const removeStyle = {
+    position: "absolute",
+    height: "15px",
+    widht: "15px"
   };
 
   return (
@@ -36,17 +57,23 @@ export default function CroppedCard(props) {
         <div>
           <img
             style={manaCrystalStyle}
-            height="80px"
-            width="80px"
             src="https://joust.hearthsim.net/branches/master/assets/images/mana_crystal.png"
             alt=""
           />
           <div style={manaStyle}>{props.manaCost}</div>
         </div>
         <div>
-          <img src={props.croppedImage} alt="" />
+          <img style={asd} src={props.croppedImage} alt="" />
         </div>
         <div style={nameStyle}>{props.name}</div>
+        <img
+          onClick={() => {
+            setCardsInDeck(cardsInDeck.remove(props.id));
+          }}
+          style={removeStyle}
+          src="https://i.dlpng.com/static/png/6774888_preview.png"
+          alt=""
+        />
       </div>
     </React.Fragment>
   );
