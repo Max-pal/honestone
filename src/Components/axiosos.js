6b18a7120acb436084edec496b96375c72d58039
SzAdrian <51090249+SzAdrian@users.noreply.github.com>
2020-05-12 00:27:55 +0200

    axios changed

--- src/Components/axiosos.js
+++ src/Components/axiosos.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+import http from "http";
 
-export const honestoneAPI = axios.create({ withCredentials: true });
+export const honestoneAPI = axios.create({
+  withCredentials: true,
+  httpAgent: new http.Agent({ keepAlive: true }),
+});
 export const blizzardAPI = axios.create();
